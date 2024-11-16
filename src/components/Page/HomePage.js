c3b6e23e4dd00862ca18cefd04b3edfb9c0c731d
Viktor Kashtanov <pistoletviktor@gmail.com>
2024-11-17 00:23:55 +0300

    seminar6

---
+++ src/components/Page/HomePage.js
@@ -0,0 +1,16 @@
+
+
+function HomePage() {
+    return ( 
+        <>
+            <h1>Главная страница</h1>
+            <h2>HomeWork 6</h2>
+            <h3>Фреймворк React JS. Погружение в Redux. Connect</h3>
+            
+        </>
+    );
+}
+
+export default HomePage;
+
+
